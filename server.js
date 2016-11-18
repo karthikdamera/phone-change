1664224d519547ab81eac80c1ec23e09552e7834
karthikdamera <karthikd@moldtekindia.com>
2016-11-18 17:09:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+var url = 'https://phone-change-contact.herokuapp.com/';
+
+http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var fbResponse = JSON.parse(body);
+        console.log("Got a response: ", fbResponse.picture);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
+
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
